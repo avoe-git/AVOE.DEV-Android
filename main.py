c4b8c8771834547ce1b52e5e146284d5de7e0321
avoe <inuser027@gmail.com>
2024-02-07 04:05:41 +0400

    Testing it already

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import flet as ft
 def main(page: ft.Page):
     page.title('Just avoe')
 
+    page.add(
+    ft.Row(controls=[
+        ft.Text("A"),
+        ft.Text("B"),
+        ft.Text("C")
+    ])
+)
+    
+    page.update()
+
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
